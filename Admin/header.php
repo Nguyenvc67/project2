73d719fb07f51de7628e9c73ab76b96824464fc8
Nguyenvc67 <vucongnguyen8@gmail.com>
2023-12-22 17:55:01 +0700

    init project

---
+++ Admin/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Sheets/style.css">
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="container">
+            <ul class="list_contact">
+                <li>
+                    Hotline: 123456789
+                </li>
+                <li>
+                    Email: gamil.com
+                </li>
+                <li>
+                    Website: www.chimtogayooo.com.vn
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+        <div class="container">
+
+            <a class="navbar-brand" href="#">
+                <img src="../Image/logo.jpg" alt="Logo" class="rounded-circle" style="width:50px; height:50px">
+            </a>
+
+            <div class="collapse navbar-collapse text-white" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Trang chủ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Thực đơn</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Khuyến mãi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Dịch vụ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tin tức</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Liên lạc</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="nav-collapse justify-content-end">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Đăng nhập</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Đăng kí</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html>
---
+++ Admin/index.php
@@ -0,0 +1,5 @@
+<?php
+include "../Model/Pdo.php";
+include "header.php";
+include "body.php";
+include "footer.php";
