53c380c11b6c1f832d82a827314faca0e38a3e22
Nguyenvc67 <vucongnguyen8@gmail.com>
2023-12-22 19:22:36 +0700

    init project

--- Admin/header.php
+++ Admin/header.php
@@ -11,7 +11,7 @@
 
 <body>
     <nav class="navbar">
-        <div class="container">
+        <div class="container d-inline">
             <ul class="list_contact">
                 <li>
                     Hotline: 123456789
@@ -69,3 +69,80 @@
 </body>
 
 </html>
+
+
+<header>
+    <div class="wrapper">
+        <nav class="navbar navbar-dark navbar-expand-lg bg-navbar fixed-top text-uppercase py-0">
+            <div class="container"><button class="navbar-toggler text-white border-0 px-2" type="button" data-bs-toggle="offcanvas" name="mobile_menu" data-bs-target="#demo"><span class="pb-2 text-white"><img loading="lazy" src="https://vtcmobile.vn/themes/vtcmobile2023/img/icon-menu.webp" alt="VTC Mobile Mobile Menu Icon" width="21"></span></button><a class="navbar-brand" href="https://vtcmobile.vn/"><img loading="lazy" src="https://vtcmobile.vn/themes/vtcmobile2023/img/logo-vtcmobile.webp" alt="VTC Mobile Logo" width="175"></a>
+                <div class="d-block d-lg-none px-2 language"><a class="dropdown-toggle text-white text-decoration-none" href="#" role="button" data-bs-toggle="dropdown">VIE</a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="#"><img loading="lazy" src="https://vtcmobile.vn/themes/vtcmobile2023/img/flag-vie.webp" width="24" alt="Tiếng Việt">Tiếng Việt</a></li>
+                        <li><a class="dropdown-item" href="#"><img loading="lazy" src="https://vtcmobile.vn/themes/vtcmobile2023/img/flag-english.webp" width="24" alt="English">English</a></li>
+                        <li><a class="dropdown-item" href="#"><img loading="lazy" src="https://vtcmobile.vn/themes/vtcmobile2023/img/flag-china.webp" width="24" alt="Chinese">简体中文</a></li>
+                    </ul>
+                </div>
+                <div class="collapse navbar-collapse justify-content-end d-none d-lg-block" id="mynavbar">
+                    <ul class="menu navbar-nav align-items-center">
+                        <li class="nav-item item-menu "><a class="nav-link text-white text-uppercase" href="https://vtcmobile.vn" target="_self" title="Trang chủ">Trang chủ</a></li>
+                        <li class="nav-item item-menu "><a class="nav-link text-white text-uppercase" href="https://vtcmobile.vn/ve-chung-toi" target="_self" title="Về chúng tôi">Về chúng tôi</a></li>
+                        <li class="nav-item item-menu dropdown ims active "><a class="nav-link" href="javascript:void(0)" target="_self" role="button" data-bs-toggle="dropdown" title="Sản phẩm">Sản phẩm <span class="dropdown-toggle"></span></a>
+                            <ul class="sub-menu dropdown-menu">
+                                <li><a class="dropdown-item border-bottom active " href="https://vtcmobile.vn/vtc-mobile-game" target="_self title=" vtc="" mobile="" game"="">VTC Mobile Game</a></li>
+                                <li><a class="dropdown-item border-bottom " href="https://vtcmobile.vn/vtc-lottery" target="_self title=" vtc="" lottery"="">VTC Lottery</a></li>
+                                <li><a class="dropdown-item border-bottom " href="https://vtcmobile.vn/vtc-esports" target="_self title=" vtc="" esports"="">VTC Esports</a></li>
+                                <li><a class="dropdown-item border-bottom " href="https://vtcmobile.vn/vtc-media" target="_self title=" vtc="" media"="">VTC Media</a></li>
+                                <li><a class="dropdown-item border-bottom " href="https://vtcmobile.vn/vtc-data-solutions" target="_self title=" data="" solutions"="">Data Solutions</a></li>
+                                <li><a class="dropdown-item " href="https://vtcmobile.vn/vtc-scoin" target="_self title=" scoin"="">Scoin</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item item-menu "><a class="nav-link text-white text-uppercase" href="https://vtcmobile.vn/tin-tuc" target="_self" title="Tin tức">Tin tức</a></li>
+                        <li class="nav-item item-menu "><a class="nav-link text-white text-uppercase" href="https://vtcmobile.vn/tuyen-dung" target="_self" title="Tuyển dụng">Tuyển dụng</a></li>
+                        <li class="nav-item item-menu "><a class="nav-link text-white text-uppercase" href="https://vtcmobile.vn/noibo" target="_blank" title="Nội bộ">Nội bộ</a></li>
+                        <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"><img src="https://vtcmobile.vn/vendor/core/core/base/images/flags/vn.svg" title="vi" width="16" alt="vi"> vi </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="https://vtcmobile.vn/en/vtc-mobile-game"><img src="https://vtcmobile.vn/vendor/core/core/base/images/flags/us.svg" title="English" width="16" alt="English"><span>English</span></a></li>
+                                <li><a class="dropdown-item" href="https://vtcmobile.vn/zh/vtc-mobile-game"><img src="https://vtcmobile.vn/vendor/core/core/base/images/flags/cn.svg" title="中文 (中国)" width="16" alt="中文 (中国)"><span>中文 (中国)</span></a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item border-start"><a class="nav-link text-white text-uppercase" href="tel:19001104">
+                                <p class="float-start mb-0 mt-2"><img loading="lazy" src="https://vtcmobile.vn/themes/vtcmobile2023/img/icon-phone.webp" width="27" alt="Hotline"></p>
+                                <p class="float-end mb-1"><span class="small fw-normal"><small>Hotline</small></span><br><span class="h5 fw-bold">19001104</span></p>
+                            </a></li>
+                    </ul>
+                </div>
+                <div class="offcanvas offcanvas-start d-block d-lg-none" id="demo">
+                    <div class="offcanvas-header p-0 border-bottom">
+                        <ul class="navbar-nav align-items-center">
+                            <li class="nav-item"><a class="navbar-brand" href="index.html"><img loading="lazy" class="p-1" src="https://vtcmobile.vn/themes/vtcmobile2023/img/logo-vtcmobile-m.webp" title="VTC Mobile" width="148"></a></li>
+                        </ul><button type="button" class="btn-close me-0" data-bs-dismiss="offcanvas"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <div class="input-group mb-3"><input type="text" class="form-control" placeholder="Tìm Kiếm"><button class="btn btn-search" type="submit"><img loading="lazy" src="https://vtcmobile.vn/themes/vtcmobile2023/img/icon-search.webp" width="26" alt="Icon"></button></div>
+                        <ul class="nav flex-column">
+                            <li class="nav-item py-2 border-bottom "><a class="nav-link" href="https://vtcmobile.vn" target="_self" title="Trang chủ">Trang chủ</a></li>
+                            <li class="nav-item py-2 border-bottom "><a class="nav-link" href="https://vtcmobile.vn/ve-chung-toi" target="_self" title="Về chúng tôi">Về chúng tôi</a></li>
+                            <li class="nav-item py-2 border-bottom active "><a class="nav-link" href="#menuId-3" target="_self" data-bs-toggle="collapse" title="Sản phẩm">Sản phẩm <span class="dropdown-toggle"></span></a>
+                                <ul class="collapse ps-2" id="menuId-3">
+                                    <li><a class="dropdown-item mobile-sub-menu-item border-bottom active " href="https://vtcmobile.vn/vtc-mobile-game" target="_self title=" vtc="" mobile="" game"="">VTC Mobile Game</a></li>
+                                    <li><a class="dropdown-item mobile-sub-menu-item border-bottom " href="https://vtcmobile.vn/vtc-lottery" target="_self title=" vtc="" lottery"="">VTC Lottery</a></li>
+                                    <li><a class="dropdown-item mobile-sub-menu-item border-bottom " href="https://vtcmobile.vn/vtc-esports" target="_self title=" vtc="" esports"="">VTC Esports</a></li>
+                                    <li><a class="dropdown-item mobile-sub-menu-item border-bottom " href="https://vtcmobile.vn/vtc-media" target="_self title=" vtc="" media"="">VTC Media</a></li>
+                                    <li><a class="dropdown-item mobile-sub-menu-item border-bottom " href="https://vtcmobile.vn/vtc-data-solutions" target="_self title=" data="" solutions"="">Data Solutions</a></li>
+                                    <li><a class="dropdown-item mobile-sub-menu-item " href="https://vtcmobile.vn/vtc-scoin" target="_self title=" scoin"="">Scoin</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item py-2 border-bottom "><a class="nav-link" href="https://vtcmobile.vn/tin-tuc" target="_self" title="Tin tức">Tin tức</a></li>
+                            <li class="nav-item py-2 border-bottom "><a class="nav-link" href="https://vtcmobile.vn/tuyen-dung" target="_self" title="Tuyển dụng">Tuyển dụng</a></li>
+                            <li class="nav-item py-2 border-bottom "><a class="nav-link" href="https://vtcmobile.vn/noibo" target="_blank" title="Nội bộ">Nội bộ</a></li>
+                            <li class="nav-item py-2"><a class="nav-link" href="tel:19001104">
+                                    <p class="float-start mb-0 mt-2"><img loading="lazy" src="https://vtcmobile.vn/themes/vtcmobile2023/img/icon-phone.webp" width="27" alt="Hotline" class="page_speed_2121733918"></p>
+                                    <p class="float-start mb-1 ms-1 d-block"><span class="small fw-normal"><small>Hotline</small></span><br><span class="h5 fw-bold d-block">19001104</span></p>
+                                </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
